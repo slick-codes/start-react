6fa44c4307e96ea0338268eb639aa0c82eb1282f
slickcodes <hartpaulisimo@gmail.com>
2022-04-17 16:07:40 +0100

    setting toggler from parent Compoent

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,22 +1,15 @@
 
-import React from "react"
+
 
 const Box = function(props){
 
-    const [theme, setTheme] = React.useState(props.darkMode)
-
-    function toggleTheme(){
-        setTheme( oldTheme => !oldTheme)
-    }
-
     const style = {
-        backgroundColor: theme? '#333' : 'transparent',
-        borderColor: theme && 'transparent'
+        backgroundColor: props.darkMode? 'black' : 'gray'
     }
 
 
     return (
-        <div className="box" onClick={toggleTheme} style={style} ></div>
+        <div className="box" onClick={props.handleThemeToggler} style={style} ></div>
     )
 }
 
