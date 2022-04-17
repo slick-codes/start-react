426f8d5aa8147f7498434ee740d412a70492e857
slickcodes <hartpaulisimo@gmail.com>
2022-04-17 07:58:30 +0100

    move meme view section to Control

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Header from './components/Header'
-import ImageContainer from './components/ImageContainer'
 
 import './App.css';
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <ImageContainer />
     </div>
   );
 }
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,34 +1,40 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import "./../assets/styles/controls.css";
 import memeObject from "./../resources/memes";
-import ImageContainer from "./ImageContainer";
+
+ 
 
 function memeGenerator() {
     const memes = memeObject.data.memes;
+    console.log(memes)
     const randomObj = memes[Math.floor(Math.random() * memes.length)];
     return randomObj.url
 }
 
 
 const Controls = function (props) {
+  
+  const memeURL = React.useState(memeGenerator())
     
-  function changeImage(){
-      <ImageContainer  url={ memeGenerator() } />
-  }
- 
-  changeImage()
-
   return (
+    <div>
       <div className="controls">
         <section>
           <input type="text" placeholder="Top Section" />
           <input type="text" placeholder="Bottom Section" />
         </section>
         <section>
-          <button onClick={changeImage} >Get a new meme Image</button>
+          <button >Get a new meme Image</button>
         </section>
       </div>
+      <div className="image__container">
+      <img src={ memeURL } alt="" />
+      <div className="title__container">
+        <h1 className="top__title"> {props.topValue} </h1>
+        <h1 className="bottom__title"> {props.bottomValue} </h1>
+      </div>
+    </div>
+    </div>
   );
 };
 
