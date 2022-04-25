3d26a502d0dc8e14f396f251269ff40a5e060d51
slickcodes <hartpaulisimo@gmail.com>
2022-04-25 13:14:03 +0100

    adding navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+
+
+function Navigation(){
+
+
+    return (
+        <nav>
+            <section>
+                Todo
+            </section>
+            <section>
+                <div className="darkmode__toggler"></div>
+                <button className="create__button">Add Todo</button>
+            </section>
+        </nav>
+    )
+}
+
+
+export default Navigation
