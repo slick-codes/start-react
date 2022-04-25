fc3ad463a57de21520b3ff19dcde45d298810416
slickcodes <hartpaulisimo@gmail.com>
2022-04-25 21:48:50 +0100

    removed searchValue props from code

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,12 +1,14 @@
 
 
 
-const SearchPanel = function(){
+const SearchPanel = function( props ){
+
+    const { handlers } = props
 
     return (
         <div className="search">
             <section className="search__bar">
-                <input type="search" placeholder="Search for for a Todo" />
+                <input type="search" onChange={ handlers.updateSearchValue } placeholder="Search for for a Todo" />
                 <button className="search__button">Search</button>
             </section>
         </div>
